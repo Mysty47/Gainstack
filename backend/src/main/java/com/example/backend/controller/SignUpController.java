71227ff269f1c4d19874424fbb6f075092905bf7
Mysty47 <106233800+Mysty47@users.noreply.github.com>
2026-07-02 12:29:47 +0300

    Docker setup, SignUp logic writes input directly into the database without exposing sensitive data in the response

--- backend/src/main/java/com/example/backend/controller/SignUpController.java
+++ backend/src/main/java/com/example/backend/controller/SignUpController.java
@@ -1,19 +1,32 @@
 package com.example.backend.controller;
 
 import com.example.backend.dto.SignUpDTO;
+import com.example.backend.dto.UserResponseDTO;
+import com.example.backend.entity.User;
+import com.example.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SignUpController {
 
+    private final UserService userService;
+
     @PostMapping("/signup")
-    public ResponseEntity<String> SignUp(@RequestBody SignUpDTO signUpDTO) {
-        if(signUpDTO.getUsername().equals("admin") && signUpDTO.getPassword().equals("admin") && signUpDTO.getEmail().equals("admin")){
-            return ResponseEntity.ok().body("Login Success");
-        }
-        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ResponseEntity<UserResponseDTO> signUp(@RequestBody SignUpDTO signUpDTO) {
+
+        User user = userService.createUser(signUpDTO);
+
+        UserResponseDTO response = new UserResponseDTO();
+        response.setId(user.getId());
+        response.setUsername(user.getUsername());
+        response.setEmail(user.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
