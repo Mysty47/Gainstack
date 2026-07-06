e65f3291e409fd00b23d98424f91c9c1ac1ffcce
Mysty47 <106233800+Mysty47@users.noreply.github.com>
2026-07-06 12:07:17 +0300

    Added workout db logic
    Now after workout /POST is called the body has more information to fill, including exerciseId, sets, reps and more. 2 new tables that store the exercises in the workouts and the sets and reps of each exercise.

--- backend/src/main/java/com/example/backend/service/WorkoutService.java
+++ backend/src/main/java/com/example/backend/service/WorkoutService.java
@@ -1,12 +1,19 @@
 package com.example.backend.service;
 
 import com.example.backend.dto.WorkoutDTO;
+import com.example.backend.dto.WorkoutExerciseDTO;
 import com.example.backend.dto.WorkoutResponseDTO;
+import com.example.backend.dto.WorkoutSetDTO;
 import com.example.backend.entity.User;
 import com.example.backend.entity.Workout;
+import com.example.backend.entity.WorkoutExercise;
+import com.example.backend.entity.WorkoutSet;
 import com.example.backend.repository.UserRepository;
 import com.example.backend.repository.WorkoutRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WorkoutService {
@@ -23,6 +30,30 @@ public class WorkoutService {
         workout.setTitle(dto.getTitle());
         workout.setWorkoutDate(dto.getWorkoutDate());
         workout.setUser(user);
+
+        List<WorkoutExercise> exercises = new ArrayList<>();
+
+        for (WorkoutExerciseDTO exDto : dto.getExercises()) {
+
+            WorkoutExercise ex = new WorkoutExercise();
+            ex.setExerciseId(exDto.getExerciseId());
+            ex.setWorkout(workout);
+
+            List<WorkoutSet> sets = new ArrayList<>();
+
+            for (WorkoutSetDTO setDto : exDto.getSets()) {
+                WorkoutSet set = new WorkoutSet();
+                set.setReps(setDto.getReps());
+                set.setWeight(setDto.getWeight());
+                set.setWorkoutExercise(ex);
+                sets.add(set);
+            }
+
+            ex.setSets(sets);
+            exercises.add(ex);
+        }
+
+        workout.setExercises(exercises);
 
         Workout saved = workoutRepository.save(workout);
 
