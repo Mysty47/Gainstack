eae8789b76e7398816d437b0284eda6e910210ed
Mysty47 <106233800+Mysty47@users.noreply.github.com>
2026-07-07 15:11:43 +0300

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,76 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import HomePage from './pages/HomePage'
-import ProfilePage from './pages/ProfilePage'
-import WorkoutPage from './pages/WorkoutPage'
-import WorkoutCreationPage from './pages/WorkoutCreationPage'
-import CreatePostPage from './pages/CreatePostPage'
-import LikedPostsPage from './pages/LikedPostsPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import ExercisesPage from "./pages/WorkoutPage.jsx";
+import WorkoutCreationPage from "./pages/WorkoutCreationPage";
+import CreatePostPage from "./pages/CreatePostPage";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/profile-page" element={<ProfilePage />} />
-        <Route path="/workout-page" element={<WorkoutPage />} />
-        <Route path="/workout-creation-page" element={<WorkoutCreationPage />} />
-        <Route path="/create-post" element={<CreatePostPage />} />
-        <Route path="/liked-posts" element={<LikedPostsPage />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          <Route
+              path="/homepage"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+          />
+
+          <Route
+              path="/profile-page"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+          />
+
+          <Route
+              path="/workout-page"
+              element={
+                <ProtectedRoute>
+                  <ExercisesPage />
+                </ProtectedRoute>
+              }
+          />
+
+          <Route
+              path="/workout-creation-page"
+              element={
+                <ProtectedRoute>
+                  <WorkoutCreationPage />
+                </ProtectedRoute>
+              }
+          />
+
+          <Route
+              path="/create-post"
+              element={
+                <ProtectedRoute>
+                  <CreatePostPage />
+                </ProtectedRoute>
+              }
+          />
+
+
+          {/* Default */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
